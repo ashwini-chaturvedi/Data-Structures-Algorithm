156f530527a24db32a0e6a8ff5e92f42b6f0baf6
Ashwini Chaturvedi <ashwinichaturvedi8924@gmail.com>
2024-08-29 10:05:33 +0530

    Update Leetcode:947. Most Stones Removed with Same Row or Column.java

--- src/src/Graphs/DisjointUnionSet/Leetcode:947. Most Stones Removed with Same Row or Column.java
+++ src/src/Graphs/DisjointUnionSet/Leetcode:947. Most Stones Removed with Same Row or Column.java
@@ -1,3 +1,9 @@
+// Approach: We will make a Union of the nodes or stones having common row or column after the union process
+             there should be distinct groups means that those groups will remain that
+             dont have any thing common neither row nor column so these stones cannot be removed and 
+             hence they will remain in the plain later to find the no. of stones removes we minus the 
+             remaining stones with total stones 
+    
 class Solution {
     public int Find(int num,int[]parents){
         if(num==parents[num]) return num;
@@ -18,25 +24,26 @@ class Solution {
         }
     }
     public int removeStones(int[][] stones) {
-        int n=stones.length;
-        int[] Parents=new int[n];
-        int[] rank=new int[n];
-        for(int i=0;i<n;i++){
+        int totalStones=stones.length;
+        int[] Parents=new int[totalStones];
+        int[] rank=new int[totalStones];
+
+        for(int i=0;i<totalStones;i++){
             Parents[i]=i;
             rank[i]=1;
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+        for(int i=0;i<totalStones;i++){
+            for(int j=i+1;j<totalStones;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){//if any row or column is common
                     Union(i,j,Parents);
                 }
             }
         }
-        int groups=0;
-        for(int i=0;i<n;i++){
+        int groups=0; //no. of groups are equal to the remaining stones 
+        for(int i=0;i<totalStones;i++){
             if(Parents[i]==i) groups++;
         }
-        return n-groups;
+        return totalStones-groups;
     }
 }
