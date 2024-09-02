3ef3b63623c2b41c7731920f053c4f18a46db189
Ashwini Chaturvedi <ashwinichaturvedi8924@gmail.com>
2024-09-02 09:46:20 +0530

    02-Sept-2024

--- src/src/Array/Leetcode:1894. Find the Student that Will Replace the Chalk.java
+++ src/src/Array/Leetcode:1894. Find the Student that Will Replace the Chalk.java
@@ -2,7 +2,33 @@
         /*Hint 1
         Subtract the sum of chalk from k until k is less than the sum of chalk.
         Hint 2
-        Now iterate over the array. If chalk[i] is less than k, this is the answer. Otherwise,                            subtract chalk[i] from k and continue.*/
+        Now iterate over the array. If chalk[i] is less than k, this is the answer. Otherwise,
+        subtract chalk[i] from k and continue.*/
+----------------------------------------------->My Solution<--------------------------------------------------
+class Solution {
+
+    public int chalkReplacer(int[] chalk, int k) {
+        int n = chalk.length;
+        long[] prefix = new long[n];
+        prefix[0] = chalk[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + chalk[i];
+        }
+
+        k %= prefix[n - 1];
+
+        int ans = -1;
+
+        for (int j = 0; j < n; j++) {
+            if (k < chalk[j]) {
+                return j;
+            }
+            k -= chalk[j];
+        }
+        return ans;
+    }
+}
+------------------------------------------------>ChatGPT<------------------------------------------------------
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
         int n=chalk.length;
