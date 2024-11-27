b70d3bd9f2c9a66fae73855e28c25d83b12c20e3
Ashwini Chaturvedi <ashwinichaturvedi8924@gmail.com>
2024-11-27 19:12:41 +0530

    27-Nov-2024

--- src/src/Interval Based Problem/Leetcode:729. My Calendar I.java
+++ src/src/Interval Based Problem/Leetcode:729. My Calendar I.java
@@ -30,14 +30,3 @@ class MyCalendar {
         return true;
     }
 }
-
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar obj = new MyCalendar();
- * boolean param_1 = obj.book(start,end);
- */
